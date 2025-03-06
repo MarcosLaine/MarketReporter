a59303e85293c5c017201251ddc4ba158be601b2
marcos laine <marcospslaine@gmail.com>
2025-03-06 13:02:19 -0300

    taking out the pushover feature

--- src/MarketReporter.py
+++ src/MarketReporter.py
@@ -24,7 +24,7 @@ market_data = market_data["Close"]
 
 # Tratar dados coletados
 market_data = market_data.dropna()
-market_data.columns = ["DOLAR", "BTC", "EURO", "IBOVESPA", "S&P500", "NASDAQ"]
+market_data.columns = ["DOLAR", "BTC", "EURO", "IBOVESPA", "sp500", "NASDAQ"]
 
 # Criar gráficos de performance
 plt.style.use("cyberpunk")
@@ -38,23 +38,26 @@ def salvar_grafico(nome, dados):
     plt.figure()
     plt.plot(dados)
     plt.title(nome)
-    plt.savefig(os.path.join(output_path, f"{nome.lower()}.png"))
+    file_path = os.path.join(output_path, f"{nome.lower()}.png")
+    plt.savefig(file_path)
     plt.close()
+
 
 # Salvar gráficos
 salvar_grafico("IBOVESPA", market_data["IBOVESPA"])
 salvar_grafico("DOLAR", market_data["DOLAR"])
-salvar_grafico("S&P500", market_data["S&P500"])
+salvar_grafico("sp500", market_data["sp500"])
 salvar_grafico("BTC", market_data["BTC"])
 salvar_grafico("EURO", market_data["EURO"])
 salvar_grafico("NASDAQ", market_data["NASDAQ"])
+
 
 # Calcular retornos diários
 daily_returns = market_data.pct_change()
 
 dolar_return = daily_returns["DOLAR"].iloc[-1]
 ibovespa_return = daily_returns["IBOVESPA"].iloc[-1]
-sp500_return = daily_returns["S&P500"].iloc[-1]
+sp500_return = daily_returns["sp500"].iloc[-1]
 btc_return = daily_returns["BTC"].iloc[-1]
 euro_return = daily_returns["EURO"].iloc[-1]
 nasdaq_return = daily_returns["NASDAQ"].iloc[-1]
@@ -82,8 +85,9 @@ def send_email_from_gmail():
         <ul>
             <li>O Ibovespa teve o retorno de {ibovespa_return}, fechando em {market_data["IBOVESPA"].iloc[-1].round(2)}.</li>
             <li>O Dólar teve o retorno de {dolar_return}, fechando em {market_data["DOLAR"].iloc[-1].round(2)}.</li>
-            <li>O S&P500 teve o retorno de {sp500_return}, fechando em {market_data["S&P500"].iloc[-1].round(2)}.</li>
+            <li>O S&P 500 teve o retorno de {sp500_return}, fechando em {market_data["sp500"].iloc[-1].round(2)}.</li>
             <li>O BTC teve o retorno de {btc_return}, preço atual em {market_data["BTC"].iloc[-1].round(2)}.</li>
+
             <li>O EURO teve o retorno de {euro_return}, fechando em {market_data["EURO"].iloc[-1].round(2)}.</li>
             <li>O NASDAQ teve o retorno de {nasdaq_return}, fechando em {market_data["NASDAQ"].iloc[-1].round(2)}.</li>
         </ul>
@@ -102,7 +106,11 @@ def send_email_from_gmail():
 
     # Anexar imagens ao e-mail
     for nome in ["ibovespa", "dolar", "sp500", "btc", "euro", "nasdaq"]:
-        with open(os.path.join(output_path, f"{nome}.png"), "rb") as img_file:
+        file_path = os.path.join(output_path, f"{nome}.png")
+        if not os.path.exists(file_path):
+            print(f"File not found: {file_path}")
+            continue  # Skip attaching this image if it doesn't exist
+        with open(file_path, "rb") as img_file:
             img = MIMEImage(img_file.read())
             img.add_header('Content-ID', f'<{nome}>')
             msg.attach(img)
@@ -124,31 +132,31 @@ def send_email_from_gmail():
 # Chamar a função para enviar o e-mail
 send_email_from_gmail()
 
-def send_pushover_notification():
-    user_key = config['pushover']['user_key']  # Get user key from config
-    api_token = config['pushover']['api_token']  # Get API token from config
-    message = f'''Relatório de Mercado do dia {datetime.now().strftime("%d/%m/%Y")}:
-    - IBOVESPA: {ibovespa_return} - Fechou em {market_data["IBOVESPA"].iloc[-1].round(2)}
-    - DÓLAR: {dolar_return} - Fechou em {market_data["DOLAR"].iloc[-1].round(2)}
-    - S&P500: {sp500_return} - Fechou em {market_data["S&P500"].iloc[-1].round(2)}
-    - BTC: {btc_return} - Preço atual em {market_data["BTC"].iloc[-1].round(2)}
-    - EURO: {euro_return} - Fechou em {market_data["EURO"].iloc[-1].round(2)}
-    - NASDAQ: {nasdaq_return} - Fechou em {market_data["NASDAQ"].iloc[-1].round(2)}
-    '''
+# def send_pushover_notification():
+#     user_key = config['pushover']['user_key']  # Get user key from config
+#     api_token = config['pushover']['api_token']  # Get API token from config
+#     message = f'''Relatório de Mercado do dia {datetime.now().strftime("%d/%m/%Y")}:
+#     - IBOVESPA: {ibovespa_return} - Fechou em {market_data["IBOVESPA"].iloc[-1].round(2)}
+#     - DÓLAR: {dolar_return} - Fechou em {market_data["DOLAR"].iloc[-1].round(2)}
+#     - S&P 500: {sp500_return} - Fechou em {market_data["sp500"].iloc[-1].round(2)}
+#     - BTC: {btc_return} - Preço atual em {market_data["BTC"].iloc[-1].round(2)}
+#     - EURO: {euro_return} - Fechou em {market_data["EURO"].iloc[-1].round(2)}
+#     - NASDAQ: {nasdaq_return} - Fechou em {market_data["NASDAQ"].iloc[-1].round(2)}
+#     '''
     
-    payload = {
-        "token": api_token,
-        "user": user_key,
-        "message": message,
-        "title": "Relatório de Mercado"
-    }
+#     payload = {
+#         "token": api_token,
+#         "user": user_key,
+#         "message": message,
+#         "title": "Relatório de Mercado"
+#     }
     
-    response = requests.post("https://api.pushover.net/1/messages.json", data=payload)
+#     response = requests.post("https://api.pushover.net/1/messages.json", data=payload)
     
-    if response.status_code == 200:
-        print("Notificação enviada com sucesso!")
-    else:
-        print(f"Erro ao enviar notificação: {response.status_code} - {response.text}")
+#     if response.status_code == 200:
+#         print("Notificação enviada com sucesso!")
+#     else:
+#         print(f"Erro ao enviar notificação: {response.status_code} - {response.text}")
 
-# Chamar a função para enviar a notificação
-send_pushover_notification()
+# # Chamar a função para enviar a notificação
+# send_pushover_notification()
